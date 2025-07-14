8bfd16dafc2e5dba2d9ef7a1cf4be2d66cfd30c2
princerxj <pr273582@gmail.com>
2025-07-14 09:14:28 +0530

    Delete Route

--- app.js
+++ app.js
@@ -57,6 +57,14 @@ app.get("/listings/:id/edit", async (req, res) => {
 app.put("/listings/:id", async (req, res) => {
     let {id} = req.params;
     await Listing.findByIdAndUpdate(id , {...req.body.listing});
+    res.redirect(`/listings/${id}`);
+})
+
+//Delete Route
+app.delete("/listings/:id", async (req, res) => {
+    let {id} = req.params;
+    let deletedListing = await Listing.findByIdAndDelete(id);
+    console.log(deletedListing);
     res.redirect("/listings");
 })
 
