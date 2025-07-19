f3788d9e9e07630f217e2f36627fe45c6b02866d
princerxj <pr273582@gmail.com>
2025-07-19 23:06:59 +0530

    Adding ExpressErrors to tackle Server and Client Errors

--- app.js
+++ app.js
@@ -7,70 +7,97 @@ const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const wrapAsync = require("./utils/wrapAsync");
+const ExpressError = require("./utils/ExpressError");
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
-app.use(express.urlencoded({extended : true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 app.engine("ejs", ejsMate);
 app.use(express.static(path.join(__dirname, "public")));
 
-main().then(() => {
+main()
+  .then(() => {
     console.log("Connected to DB");
-}).catch((err) => {
+  })
+  .catch((err) => {
     console.log(err);
-})
+  });
 
 async function main() {
-    await mongoose.connect(MONGO_URL);
+  await mongoose.connect(MONGO_URL);
 }
 
 //Index Route
-app.get("/listings", async (req, res) => {
+app.get(
+  "/listings",
+  wrapAsync(async (req, res) => {
     const allListings = await Listing.find({});
-    res.render("./listings/index.ejs", {allListings});
-});
+    res.render("./listings/index.ejs", { allListings });
+  })
+);
 
 //New Route
 app.get("/listings/new", (req, res) => {
-    res.render("./listings/new.ejs");
-})
+  res.render("./listings/new.ejs");
+});
 
 //Show Route
-app.get("/listings/:id", async (req, res) => {
-    let {id} = req.params;
+app.get(
+  "/listings/:id",
+  wrapAsync(async (req, res) => {
+    let { id } = req.params;
     const listing = await Listing.findById(id);
-    res.render("./listings/show.ejs", {listing});
-})
+    res.render("./listings/show.ejs", { listing });
+  })
+);
 
 //Create Route
-app.post("/listings", wrapAsync(async (req, res, next) => {
+app.post(
+  "/listings",
+  wrapAsync(async (req, res, next) => {
+    if(!req.body.listing) {
+        throw new ExpressError(400, "Send Valid Data");
+    }
     const newListing = new Listing(req.body.listing);
     await newListing.save();
     res.redirect("/listings");
-}));
+  })
+);
 
 //Edit Route
-app.get("/listings/:id/edit", async (req, res) => {
-    let {id} = req.params;
+app.get(
+  "/listings/:id/edit",
+  wrapAsync(async (req, res) => {
+    let { id } = req.params;
     const listing = await Listing.findById(id);
-    res.render("./listings/edit.ejs", {listing});
-});
+    res.render("./listings/edit.ejs", { listing });
+  })
+);
 
 //Update Route
-app.put("/listings/:id", async (req, res) => {
-    let {id} = req.params;
-    await Listing.findByIdAndUpdate(id , {...req.body.listing});
+app.put(
+  "/listings/:id",
+  wrapAsync(async (req, res) => {
+    if(!req.body.listing) {
+        throw new ExpressError(400, "Send Valid Data");
+    }
+    let { id } = req.params;
+    await Listing.findByIdAndUpdate(id, { ...req.body.listing });
     res.redirect(`/listings/${id}`);
-})
+  })
+);
 
 //Delete Route
-app.delete("/listings/:id", async (req, res) => {
-    let {id} = req.params;
+app.delete(
+  "/listings/:id",
+  wrapAsync(async (req, res) => {
+    let { id } = req.params;
     let deletedListing = await Listing.findByIdAndDelete(id);
     console.log(deletedListing);
     res.redirect("/listings");
-})
+  })
+);
 
 // app.get("/testListing", async (req, res) => {
 //     let sampleListing = new Listing({
@@ -87,13 +114,18 @@ app.delete("/listings/:id", async (req, res) => {
 // })
 
 app.get("/", (req, res) => {
-    res.send("Hello , I am root");
-})
-
-app.use((err, req, res, next)=> {
-    res.send("Something went Wrong");
+  res.send("Hello , I am root");
 });
 
-app.listen(8080, ()=> {
-    console.log("Server is listening to port 8080");
-})
+app.get("*", (req, res, next) => {
+  next(new ExpressError(404, "Page not found!"));
+});
+
+app.use((err, req, res, next) => {
+  let { statusCode = 500, message = "Something went wrong"} = err;
+  res.status(statusCode).send(message);
+});
+
+app.listen(8080, () => {
+  console.log("Server is listening to port 8080");
+});
