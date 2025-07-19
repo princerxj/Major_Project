b55eb7bcc0549fd91bd4ee19e05c816fcfb6434e
princerxj <pr273582@gmail.com>
2025-07-19 23:55:17 +0530

    Validate Listing

--- app.js
+++ app.js
@@ -32,6 +32,7 @@ async function main() {
 const validateListing = (req, res, next) => {
   let { error } = listingSchema.validate(req.body);
   if (error) {
+    let errMsg = error.details.map((el) => el.message).join(",");
     throw new ExpressError(400, error);
   } else {
     next();
