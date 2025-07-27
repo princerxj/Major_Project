ffb350324700afa6ec9e356217ff930c5daed840
princerxj <pr273582@gmail.com>
2025-07-27 12:15:26 +0530

    Handling simultaneous deletions of the listings and their respective reviews

--- app.js
+++ app.js
@@ -140,6 +140,16 @@ app.post(
   })
 );
 
+//Delete Review Route 
+app.delete("/listings/:id/reviews/:reviewId", wrapAsync(async(req, res) => {
+  let {id, reviewId} = req.params;
+
+  await Listing.findByIdAndUpdate(id, {$pull : {reviews : {reviewId}}});
+  await Review.findByIdAndDelete(reviewId);
+
+  res.redirect(`/listings/${id}`);
+}))
+
 // app.get("/testListing", async (req, res) => {
 //     let sampleListing = new Listing({
 //         title : "My new Villa",
